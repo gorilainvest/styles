84e034072ba69f2915edc91014e03d6c4e2e1b44
Guilherme Gois <guilherme.gois@gorilainvest.com.br>
2021-06-16 13:08:36 -0300

    refactor(colors): add and update colors in sass and js files
    - add pension asset class colors
    - add missing button colors
    - add tertiary button colors
    - update multimarket colors
    - sync asset class colors between sass and js files

--- js/Colors.d.ts
+++ js/Colors.d.ts
@@ -28,6 +28,8 @@ declare namespace _default {
     export const OffshoreSeconday: string;
     export const CashPrimary: string;
     export const CashSecondary: string;
+    export const PensionPrimary: string;
+    export const PensionSecondary: string;
     export const Positive: string;
     export const Negative: string;
     export const BuyPrimary: string;
@@ -35,7 +37,15 @@ declare namespace _default {
     export const SellPrimary: string;
     export const SellSecondary: string;
     export const PrimaryButton: string;
+    export const PrimaryButtonFontColor: string;
+    export const PrimaryButtonHover: string;
     export const SecondaryButton: string;
+    export const SecondaryButtonFontColor: string;
+    export const SecondaryButtonHover: string;
+    export const TertiaryButton: string;
+    export const TertiaryButtonFontColor: string;
+    export const TertiaryButtonHover: string;
     export const DisabledButton: string;
+    export const DisabledButtonFontColor: string;
 }
 export default _default;
